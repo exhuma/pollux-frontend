7ef52e9ae27f54a529d302a5379e19298da3fb04
Michel Albert <michel@albert.lu>
2021-06-29 13:16:54 +0000

    Add "fab run" task

--- fabfile.py
+++ fabfile.py
@@ -4,3 +4,8 @@ from fabric import task
 def develop(context):
     context.run("npm install", replace_env=False, pty=True)
     context.run("pre-commit install", replace_env=False)
+
+
+@task
+def run(context):
+    context.run("npm run serve", replace_env=False, pty=True)
